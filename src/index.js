3579c85cc4e12a368ee2dd99d534ce5088c54bef
Michael <micamerchan@gmail.com>
2022-08-15 11:13:02 -0500

    First Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,107 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const data = {
+  Colombia: "Bogota",
+  Alemania: "Berlin",
+  Mexico: "Ciudad de Mexico",
+};
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function getArray(currentData) {
+  const countries = Object.keys(currentData);
+  const capitals = Object.values(currentData);
+  return countries.concat(capitals).sort(() => Math.random() - 0.5);
+}
+
+function ListElement(props) {
+  return (
+    <button
+      className={props.oneSelected === props.value ? "selected" : ""}
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
+}
+
+class List extends React.Component {
+  render() {
+    return (
+      <>
+        {this.props.data.map((current) => (
+          <>
+            <ListElement
+              key={current}
+              value={current}
+              onClick={() => this.props.onClick(current)}
+              oneSelected={this.props.oneSelected}
+            />{" "}
+          </>
+        ))}
+      </>
+    );
+  }
+}
+
+class Game extends React.Component {
+  onClick(value) {
+    if (!this.state.oneSelected) {
+      this.setState({ oneSelected: value });
+    } else {
+      if (
+        Object.keys(data).includes(value) ||
+        Object.keys(data).includes(this.state.oneSelected)
+      ) {
+        if (
+          data[value] === this.state.oneSelected ||
+          data[this.state.oneSelected] === value
+        ) {
+          const lastValue = this.state.oneSelected;
+          this.setState({
+            currentData: this.state.currentData.slice().filter(function (item) {
+              return item !== lastValue && item !== value;
+            }),
+            oneSelected: null,
+          });
+        } else {
+          this.setState({ oneSelected: null });
+        }
+      } else {
+        this.setState({ oneSelected: null });
+      }
+    }
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      oneSelected: null,
+      currentData: getArray(data),
+    };
+  }
+  render() {
+    if (this.state.currentData.length > 0) {
+      return (
+        <div className="game-board">
+          <List
+            data={this.state.currentData}
+            onClick={(value) => this.onClick(value)}
+            oneSelected={this.state.oneSelected}
+          />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div>Congrats! You won</div>
+        </div>
+      );
+    }
+  }
+}
+
+// ========================================
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Game />);
